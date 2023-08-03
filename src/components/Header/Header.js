1600929d287bc32f3c82bbc31c8841b7ca863cef
Irina Demina <ir-mars@yandex.ru>
2023-08-03 15:29:29 +0600

    Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,46 +8,45 @@ import Navigation from '../Navigation/Navigation';
 function Header ({ menuOpened, handleMenuOpened }) {
   
   return (
-    <header className='header'>
+    <header className="header">
       <Routes>
-        
         <Route path="/" element={
           <>
-          <div className="header__container">
-            <img className="header__logo" src={headerlogo} alt="логотип" />
+          <div className="header__wrapper header__wrapper_main">
+            <img className="header__logo" src={headerlogo} alt="логотип в виде зеленого круга" />
             <nav className="header__nav-links">
-              <Link to="/sign-up" className="header__link header__link_auth">Регистрация</Link>
-              <Link to="/sign-in" className="header__link header__link_auth header__link_type_btn">Войти</Link>
+              <Link to="/sign-up" className="header__link header__link_reg">Регистрация</Link>
+              <Link to="/sign-in" className="header__link header__link_reg header__link_btn">Войти</Link>
             </nav>
           </div>  
           </>
         } />
         
         <Route path="*" element={
-          <>
-            <img className="header__logo" src={logo} alt="логотип" />
-            <nav className="header__nav-links header__nav-links_hidden_tablet-mobile">            
-              <Link to="/movies" className='header__link'>
+          <div className="header__wrapper">
+            <nav className="header__nav-links header__nav-links_hidden_tab-mobile">            
+              <img className="header__logo" src={logo} alt="логотип в виде морды кота" />              
+            </nav>
+            <nav className="header__nav-links header__nav-links_hidden_tab-mobile">
+              <Link to="/movies" className="header__link">
                 Фильмы
               </Link>
-              <Link to="/saved-movies" className='header__link header__link_medium'>
+              <Link to="/saved-movies" className="header__link header__link_savefilms">
                 Сохранённые фильмы
               </Link>
-            </nav>
-            <nav className='header__nav-links header__nav-links_hidden_tablet-mobile'>
-              <Link to="/profile" className='header__link header__link_profile'>
+              <Link to="/profile" className="header__link header__link_acc">
                 Аккаунт
-                <div className='header__nav-link-icon'></div>
+                <div className="header__acc-icon"></div>
               </Link>
             </nav>  
 
-            <nav className='header__nav-links header__nav-links_hidden_pc'>
+            <nav className="header__nav-links header__nav-links_hidden_pc">
               <img className="header__logo" src={logo} alt="логотип" />
-              <button className={`header__btn-menu ${menuOpened ? 'header__btn-menu_close' : ''}`} onClick={() => handleMenuOpened(!menuOpened)}>
+              <button className={`header__burger-btn ${menuOpened ? "header__burger-btn_close" : ''}`} onClick={() => handleMenuOpened(!menuOpened)}>
               </button>
             </nav>
             <Navigation menuOpened={menuOpened} onClose={() => handleMenuOpened(false)} />
-          </>
+          </div>
         } />
       </Routes>
     </header>
